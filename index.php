537f64eb729ad338a316ae455565ea4f2b529093
Nicholas Vitovitch <ngvitovitch@gmail.com>
2012-09-08 14:52:37 -0400

    Added bootstrap files and a skeleton of a main page.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Piñata</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="The internet time-capsule">
+		
+		<!-- Le styles -->
+		<link href="./css/bootstrap.css" rel="stylesheet">
+	</head>
+	
+	<body style="padding:80px">
+		<!-- Nav Bar -->
+		<div class="navbar navbar-fixed-top" style="position: absolute;">
+			<div class="navbar-inner">
+				<div class="container" style="width: auto; padding: 0 20px;">
+					<a class="brand" href="#">Piñata</a>
+					<ul class="nav">
+						<li><a href="#">Account</a></li>
+						<li><a href="#">Candy Bowl</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Welcome message -->
+		<div class="hero-unit">
+			<h1>Hola, Interwebs!</h1>
+			<p>Piñata blah blah blah blah...</p>
+			<p>
+				<a class="btn btn-primary btn-large">
+					Ok, thanks!
+				</a>
+			</p>
+		</div>
+	</body>
+</html>
